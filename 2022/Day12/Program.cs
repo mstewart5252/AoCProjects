d52da56ff0718ec8369b66426962270087278cdc
Michael Stewart <mstewart@tangerineglobal.com>
2022-12-12 22:42:21 -0800

    working solution for day12, could use some refactoring

--- 2022/Day12/Program.cs
+++ 2022/Day12/Program.cs
@@ -11,46 +11,131 @@ public static class Program
         FindOnGrid(file, out var startNode, out var endLocation);
         openNodes.Add(startNode);
 
+        //Part1
+        var finalNode = AStarSearch(openNodes, closedNodes, endLocation, file);
+        var steps = finalNode.GetSteps();
+        Console.WriteLine($"Part 1: {steps}");
+        
+        //Part2
+        openNodes = new List<Node>();
+        closedNodes = new List<Node>();
+        openNodes.Add(new Node(endLocation));
+        var firstLowestNode = ReverseAStarSearch(openNodes, closedNodes, file);
+        steps = firstLowestNode.GetSteps();
+        Console.WriteLine($"Part 2: {steps}");
+    }
+
+    private static Node? ReverseAStarSearch(List<Node> openNodes, List<Node> closedNodes, string[] file)
+    {
         Node? finalNode = null;
-        do
+
+        while (openNodes.Any())
         {
             var currentNode = openNodes.OrderByDescending(n => n.TravelScore).First();
             if (currentNode == null)
                 throw new Exception();
+
             openNodes.Remove(currentNode);
             closedNodes.Add(currentNode);
 
-            if (!currentNode.GridSquare.Location.Equals(endLocation.Location))
+            if (currentNode.GridSquare.Elevation.Equals('a'))
             {
-                var neighbors = GetNeighbors(currentNode, endLocation, file);
-                foreach (var neighbor in neighbors)
+                if (finalNode == null || GetSteps(finalNode) > GetSteps(currentNode.Parent))
+                    finalNode = currentNode.Parent;
+                break;
+            }
+
+            foreach (var neighbor in GetNeighbors(currentNode, file))
+            {
+                if (!IsTraversable(neighbor.GridSquare, currentNode.GridSquare))
+                    continue;
+
+                var oldNode = closedNodes.FirstOrDefault(n => n.GridSquare.Location.Equals(neighbor.GridSquare.Location));
+                if (oldNode != null)
                 {
-                    //I think it's something here
-                    if (!closedNodes.Any(n => n.GridSquare.Location.Equals(neighbor.GridSquare.Location)) && 
-                        IsTraversable(currentNode.GridSquare, neighbor.GridSquare))
+                    if (neighbor.DistanceFromStart < oldNode.DistanceFromStart)
+                        oldNode.UpdateParent(currentNode);
+                }
+                else
+                {
+                    oldNode = openNodes.FirstOrDefault(n => n.GridSquare.Location.Equals(neighbor.GridSquare.Location));
+                    if (oldNode != null)
                     {
-                        if (!openNodes.Any(n => n.GridSquare.Location.Equals(neighbor.GridSquare.Location)))
-                            openNodes.Add(neighbor);
-                        else
-                        {
-                            var oldNode = openNodes.First(n => n.GridSquare.Location.Equals(neighbor.GridSquare.Location));
-                            if (neighbor.TravelScore < oldNode.TravelScore)
-                                oldNode.UpdateParent(currentNode);
-                        }
+                        if (neighbor.GetSteps() < oldNode.GetSteps())
+                            oldNode.UpdateParent(currentNode);
+                    }
+                    else
+                    {
+                        openNodes.Add(neighbor);
                     }
                 }
             }
-            else
-                finalNode = currentNode.Parent;
-        } while (finalNode == null);
+        }
 
-        var steps = 0;
-        do
+        return finalNode;
+    }
+
+    private static Node? AStarSearch(List<Node> openNodes, List<Node> closedNodes, GridSquare endLocation, string[] file)
+    {
+        Node? finalNode = null;
+
+        while (openNodes.Any())
         {
-            finalNode = finalNode.Parent;
+            var currentNode = openNodes.OrderByDescending(n => n.TravelScore).First();
+            if (currentNode == null)
+                throw new Exception();
+
+            openNodes.Remove(currentNode);
+            closedNodes.Add(currentNode);
+
+            if (currentNode.GridSquare.Location.Equals(endLocation.Location))
+            {
+                if (finalNode == null || GetSteps(finalNode) > GetSteps(currentNode.Parent))
+                    finalNode = currentNode.Parent;
+                break;
+            }
+
+            foreach (var neighbor in GetNeighbors(currentNode, file))
+            {
+                if (!IsTraversable(currentNode.GridSquare, neighbor.GridSquare))
+                    continue;
+
+                var oldNode = closedNodes.FirstOrDefault(n => n.GridSquare.Location.Equals(neighbor.GridSquare.Location));
+                if (oldNode != null)
+                {
+                    if (neighbor.DistanceFromStart < oldNode.DistanceFromStart)
+                        oldNode.UpdateParent(currentNode);
+                }
+                else
+                {
+                    oldNode = openNodes.FirstOrDefault(n => n.GridSquare.Location.Equals(neighbor.GridSquare.Location));
+                    if (oldNode != null)
+                    {
+                        if (neighbor.GetSteps() < oldNode.GetSteps())
+                            oldNode.UpdateParent(currentNode);
+                    }
+                    else
+                    {
+                        openNodes.Add(neighbor);
+                    }
+                }
+            }
+        }
+
+        return finalNode;
+    }
+
+    private static int GetSteps(this Node? node)
+    {
+        var steps = 0;
+        var nodeTemp = node;
+        while (nodeTemp != null)
+        {
+            nodeTemp = nodeTemp.Parent;
             steps++;
-        } while (finalNode != null);
-        Console.WriteLine(steps);
+        }
+
+        return steps;
     }
 
     private static bool IsTraversable(GridSquare locationA, GridSquare locationB)
@@ -60,7 +145,6 @@ public static class Program
     }
 
     private static List<Node> GetNeighbors(Node currentNode,
-        GridSquare endLocation,
         string[] input)
     {
         var neighborLocations = new List<Tuple<int, int>>();
@@ -100,7 +184,7 @@ public static class Program
         foreach (var neighborLocation in neighborLocations)
         {
             output.Add(new Node(new GridSquare(neighborLocation, 
-                input[neighborLocation.Item1][neighborLocation.Item2]), endLocation, currentNode));
+                input[neighborLocation.Item1][neighborLocation.Item2]), currentNode));
         }
 
         return output;
@@ -121,7 +205,7 @@ public static class Program
 
                 if (startTemp != null && endTemp != null)
                 {
-                    startNode = new Node(startTemp, endTemp);
+                    startNode = new Node(startTemp);
                     endLocation = endTemp;
                     return;
                 }
@@ -153,15 +237,20 @@ public class Node
 {
     public Node? Parent { get; set; }
     public GridSquare GridSquare { get; set; }
-    public int DistanceFromStart { get; set; }
-    public int DistanceToEnd { get; set; }
-    public int TravelScore { get; set; }
+    public double DistanceFromStart { get; set; }
+    public double DistanceToEnd { get; set; }
+    public double TravelScore { get; set; }
 
-    public Node(GridSquare thisLocation, GridSquare endLocation, Node? parent = null)
+    public Node(GridSquare thisLocation, Node? parent = null)
     {
         GridSquare = thisLocation;
         DistanceFromStart = parent?.DistanceFromStart + 1 ?? 0;
-        DistanceToEnd = GetDistance(thisLocation, endLocation);
+        //DistanceToEnd = GetDistance(thisLocation, endLocation);
+        
+        //Using arbitrary number close to distance from start at end of testing
+        //GetDistance was not accurate due to obstacles
+        //This is no longer a pure heuristic search, but it works in this case
+        DistanceToEnd = 350 - DistanceFromStart;
         TravelScore = DistanceFromStart + DistanceToEnd;
         if (parent != null)
             Parent = parent;
@@ -174,10 +263,11 @@ public class Node
         TravelScore = DistanceFromStart + DistanceToEnd;
     }
 
-    private int GetDistance(GridSquare start, GridSquare target)
+    private double GetDistance(GridSquare start, GridSquare target)
     {
-        var a = target.Location.Item1 - start.Location.Item1;
-        var b = target.Location.Item2 - start.Location.Item2;
+        var a = Math.Abs(target.Location.Item1 - start.Location.Item1);
+        var b = Math.Abs(target.Location.Item2 - start.Location.Item2);
+        
         return a + b;
     }
 }
